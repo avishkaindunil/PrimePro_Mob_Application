a69f4a0c8fe11c6c93432f66a7f51dbc2f64acf0
avishkaindunil <haavishkaindunil@gmail.com>
2024-07-31 13:58:34 +0530

    tabs created

---
+++ app/(tabs)/activities.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+//explore
+export default function activities() {
+  return (
+    <View>
+      <Text>activities</Text>
+    </View>
+  )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  useFonts({
+    'mulish':require('./../assets/fonts/Mulish-Regular.ttf'),
+    'mulish-medium':require('./../assets/fonts/Mulish-Medium.ttf'),
+    'mulish-semibold':require('./../assets/fonts/Mulish-SemiBold.ttf')
+  })
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack screenOptions={{
+      headerShown:false
+    }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
