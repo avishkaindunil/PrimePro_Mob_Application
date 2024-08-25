f0a28221169c7c7e0563efd5293f6efe04457414
avishkaindunil <haavishkaindunil@gmail.com>
2024-08-25 22:54:59 +0530

    about us created

---
+++ app/pages/about-us.jsx
@@ -0,0 +1,7 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Feather from '@expo/vector-icons/Feather';
+import * as ImagePicker from 'expo-image-picker';
